356a46b9d1df587e55f3dd8d024ad6d711f68531
mgkyawmin <kyawmintun.kmt4142020@gmail.com>
2025-04-19 21:43:24 +0700

    change line id

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
                 <a target="_blank" href="https://www.facebook.com/profile.php?id=100012289154309" title="facebook" rel="noreferrer"><i className="fa fa-facebook" aria-hidden="true"></i></a>
                 <a target="_blank" href="https://wa.me/959794144487" title="whatsapp" rel="noreferrer"><i className="fa fa-whatsapp" aria-hidden="true"></i></a>
                 {/* <a target="_blank" href="https://www.linkedin.com/in/kyaw-min-tun-788a5a264/" title="linkedin"><i className="fa fa-linkedin" aria-hidden="true"></i></a> */}
-                <a target="_blank" href="https://line.me/ti/p/~kmt24" title="line" rel="noreferrer"><i className="bi bi-line"></i></a>
+                <a target="_blank" href="https://line.me/ti/p/~kmt96" title="line" rel="noreferrer"><i className="bi bi-line"></i></a>
             </div>
 
 
